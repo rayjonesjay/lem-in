534eb8933266126f665c3803f818432d413f4c88
rayjonesjay <rayjaymuiruri@gmail.com>
2024-12-03 09:17:19 +0300

    add: errors package

--- xerrors/err.go
+++ xerrors/err.go
@@ -1,12 +1,17 @@
 // package xerrors contains error types and error handling functions for efficient error handling
 package xerrors
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
 
 // all error variables must have the perfix Err
 // ErrStartNotFound and ErrEndNotFound will have exit status of 3
 // ErrNoArgsPassed exits with status 1
 // ErrInvalidFileContents exits with status 4
+// ErrMaxAntNumExceeded exits with status 1
 var (
 	// returned when ##start instruction is missing in input file, exit with status 3
 	ErrStartNotFound = errors.New("StartNotFound: %s does not contain ##start instruction")
@@ -18,5 +23,18 @@ var (
 	ErrNoArgsPassed = errors.New("NoArgsPassed: no arguments passed")
 
 	// returned when a file contains invalid characters such as emojis,characters whose ascii value > 127, values which are not letters , # or numbers.
-	ErrInvalidFileContents = errors.New("ERROR: InvalidFileContents: %s contains invalid file contents, valid contents include numbers[0-9],letters[A-Z], #, and -")
+	ErrInvalidFileContents = errors.New("InvalidFileContents: %s contains invalid file contents, valid contents include numbers[0-9],letters[A-Z], #, and -")
+
+	// returned when ant numbers surpass a given value, default is 1000 ants
+	ErrMaxAntNumExceeded = errors.New("MaxAntNumExceeded: number of ants exceeded try lower than %d")
+
+	// returned when the number of ants is 0
+	ErrZeroAnts = errors.New("ZeroAnts: no ants in the colony")
 )
+
+func ErrorWriter(err error, exitCode int, shouldExit bool) {
+	fmt.Println(err)
+	if shouldExit {
+		os.Exit(exitCode)
+	}
+}
