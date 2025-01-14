7fa9e528b4b5ef460abdb8d825bb0532029f1e06
josie-opondo <josephineopondo5@gmail.com>
2025-01-14 22:10:02 +0300

    Add NewParser constructor to initialize parser with room and tunnel data structures.

--- controllers/parser.go
+++ controllers/parser.go
@@ -1,1 +1,17 @@
 package controllers
+
+import (
+	"lemin/models"
+)
+
+type Parser struct {
+	rooms   map[string]*models.Room
+	tunnels []*models.Tunnel
+}
+
+func NewParser() *Parser {
+	return &Parser{
+		rooms:   make(map[string]*models.Room),
+		tunnels: make([]*models.Tunnel, 0),
+	}
+}
