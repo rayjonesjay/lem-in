5eacf30bacced05ef12d2ffc84bebede425e7e38
josie-opondo <josephineopondo5@gmail.com>
2025-01-14 08:13:22 +0300

    feat(models): add SetPath method for Ant struct

--- models/ant.go
+++ models/ant.go
@@ -7,10 +7,17 @@ type Ant struct {
 	PathIdx  int
 }
 
+// Create a new ant with the given ID and starting room
 func NewAnt(id int, startRoom *Room) *Ant {
 	return &Ant{
 		ID:       id,
 		Position: startRoom,
 		PathIdx:  0,
 	}
+}
+
+// Move ant to the next room in the path
+func (a *Ant) SetPath(path []*Room) {
+	a.Path = path
+	a.PathIdx = 0
 }
