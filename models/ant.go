84bbae5939d6c196f3265d62a650372cd0009839
josie-opondo <josephineopondo5@gmail.com>
2025-01-14 08:10:20 +0300

    feat(models): add Ant struct with initialization logic

--- models/ant.go
+++ models/ant.go
@@ -1,1 +1,16 @@
 package models
+
+type Ant struct {
+	ID       int
+	Position *Room
+	Path     []*Room
+	PathIdx  int
+}
+
+func NewAnt(id int, startRoom *Room) *Ant {
+	return &Ant{
+		ID:       id,
+		Position: startRoom,
+		PathIdx:  0,
+	}
+}
