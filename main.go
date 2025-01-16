e9148e69b6f53d56a77d72eb08147afa88c10448
rayjonesjay <rayjaymuiruri@gmail.com>
2025-01-17 00:45:07 +0300

    read file from cmd and parse

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"lemin/controllers"
+)
 
 func main() {
 	fmt.Println("let them in ")
+	// read the file name from the command line
+	arguments := os.Args[1:]
+	if len(arguments) != 1 {
+		log.Println("ERROR: lem-in expects one argument")
+	}
+
+	// call the parser function to read the file data
+	controllers.NewParser().ParseFile(arguments[0])
+	
 }
