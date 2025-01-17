4337ccbe6cd8e79776485745c9c3560b054f90bf
Wambita <wambitafana@gmail.com>
2025-01-17 13:37:14 +0300

    Chore: remove print statements and commented code

--- controllers/pathfinder.go
+++ controllers/pathfinder.go
@@ -57,47 +57,5 @@ func PathFinder(colony models.Colony) ([][]string, error) {
 		return len(paths[i]) < len(paths[j])
 	})
 
-	fmt.Println(paths)
 	return paths, nil
 }
-
-// func main() {
-// 	// Manually creating a colony for testing
-// 	colony := models.Colony{
-// 		Rooms: make(map[string]*models.Room),
-// 	}
-
-// 	// Manually creating rooms
-// 	startRoom := &models.Room{Name: "A", Neighbours: []*models.Room{}}
-// 	endRoom := &models.Room{Name: "D", Neighbours: []*models.Room{}}
-// 	roomB := &models.Room{Name: "B", Neighbours: []*models.Room{}}
-// 	roomC := &models.Room{Name: "C", Neighbours: []*models.Room{}}
-
-// 	// Connecting rooms manually (defining neighbors)
-// 	startRoom.Neighbours = append(startRoom.Neighbours, roomB)
-// 	roomB.Neighbours = append(roomB.Neighbours, startRoom, roomC)
-// 	roomC.Neighbours = append(roomC.Neighbours, roomB, endRoom)
-// 	endRoom.Neighbours = append(endRoom.Neighbours, roomC)
-
-// 	// Adding rooms to the colony
-// 	colony.Rooms["A"] = startRoom
-// 	colony.Rooms["B"] = roomB
-// 	colony.Rooms["C"] = roomC
-// 	colony.Rooms["D"] = endRoom
-
-// 	// Marking start and end rooms in the colony
-// 	colony.StartRoom = *startRoom
-// 	colony.EndRoom = *endRoom
-// 	colony.StartFound = true
-// 	colony.EndFound = true
-
-// 	// Call PathFinder
-// 	paths, err := PathFinder(colony)
-// 	if err != nil {
-// 		fmt.Println("Error:", err)
-// 		return
-// 	}
-
-// 	// Print the found paths
-// 	fmt.Println("Paths found:", paths)
-// }
