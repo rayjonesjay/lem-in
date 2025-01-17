6f2c6e404b3471b7b042b4b2e9aa76407d1b1b62
rayjonesjay <rayjaymuiruri@gmail.com>
2025-01-18 01:04:53 +0300

    change: reading and parsing file contents have designated functions each

--- controllers/readFile.go
+++ controllers/readFile.go
@@ -1,1 +1,60 @@
 package controllers
+
+import (
+	"bufio"
+	"errors"
+	"os"
+	"strings"
+	"unicode"
+)
+
+// ReadValidateInputFile reads the data from the file passed as a command line argument
+// It checks if the file contains only ascii characters
+// It checks if the file passed exists
+// It returns err if any of the checks fail
+func ReadValidateInputFile(filename string) (fileContents []string, err error) {
+
+	fd, err := os.Open(filename)
+
+	if err != nil {
+		return nil, err
+	}
+	defer fd.Close()
+
+	scanner := bufio.NewScanner(fd)
+
+	for scanner.Scan() {
+		currentLine := scanner.Text()
+
+		// check if the read line contains only valid ASCII characters and skips it
+		if !ContainsASCII(currentLine) {
+			continue
+		}
+
+		// remove any trailing or leading white spaces
+		currentLine = strings.TrimSpace(currentLine)
+
+		// ignore empty line
+		if currentLine == "" {
+			continue
+		}
+
+		fileContents = append(fileContents, currentLine)
+	}
+
+	if len(fileContents) == 0 {
+		return nil, errors.New("no valid input file found")
+	}
+
+	return
+}
+
+func ContainsASCII(s string) bool {
+	for _, c := range s {
+		// checks if c is greater than 127
+		if c > unicode.MaxASCII {
+			return false
+		}
+	}
+	return true
+}
