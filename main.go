99eac6c839f41006a6593cec1ebae7b500a3420c
WycliffeAlphus <wycliffealphus7@gmail.com>
2025-01-18 23:04:02 +0300

    feat:added the functions in the main

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"lemin/controllers"
+	"os"
+)
 
 func main() {
-	fmt.Println("let them in ")
+
+	if len(os.Args) != 2 {
+		fmt.Println("Expecting: go run . [name of file]")
+		return
+	}
+	p := controllers.NewParser()
+
+	c, err := p.ParseFile(os.Args[1])
+	fmt.Println("Parsing file...")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(c)
+	fmt.Println("Success")
+
+	fmt.Println("Initializing ants to paths..")
+	controllers.InitializeAnts(c)
+	fmt.Println("Initialization successful")
+
+	fmt.Println("Starting the movement...let them in ")
+
+	mover := controllers.NewMover(c)
+	movements := mover.ExecuteMovements()
+
+	fmt.Println(movements)
 }
