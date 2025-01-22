bd573b906b26fefd930afe9ada97f7145045f684
wyonyango <wycliffealphus7@gmail.com>
2025-01-22 09:26:46 +0300

    refactor:remove redundant code

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 
@@ -10,27 +9,18 @@ import (
 
 func main() {
 	if len(os.Args) != 2 {
-		fmt.Println("Expecting: go run . [name of file]")
+		log.Println("ERROR: lem-in expects one argument\nExpecting: go run . [name of file]")
 		return
 	}
 	p := controllers.NewParser()
-	// fmt.Println("Parsing file...")
+
 	c, err := p.ParseFile(os.Args[1])
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 		return
 	}
 
 	controllers.InitializeAnts(c)
 
 	controllers.Mover(c)
-	
-	// read the file name from the command line
-	arguments := os.Args[1:]
-	if len(arguments) != 1 {
-		log.Println("ERROR: lem-in expects one argument")
-	}
-
-	// call the parser function to read the file data
-	controllers.NewParser().ParseFile(arguments[0])
 }
